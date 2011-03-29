e5ef7f7eb1b92d64980f6a179a3f8251da35ca3b
Alexandre Porcelli <alexandre.porcelli@gmail.com>
2011-03-29 08:48:18 -0300

    wip

---
+++ src/test/java/org/mule/AppTest.java
@@ -0,0 +1,19 @@
+
+package org.mule;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.matchers.JUnitMatchers.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest  {
+
+  @Test
+  public void testApp() {
+    assertTrue( true );
+  }
+}
