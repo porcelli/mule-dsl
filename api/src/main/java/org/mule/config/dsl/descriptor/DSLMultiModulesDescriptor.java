8ed521ae8ac7c139941b74c25630e112e723c66c
Alexandre Porcelli <alexandre.porcelli@gmail.com>
2011-09-30 02:02:49 -0300

    ops.. missing files ;)

---
+++ api/src/main/java/org/mule/config/dsl/descriptor/DSLMultiModulesDescriptor.java
@@ -0,0 +1,11 @@
+package org.mule.config.dsl.descriptor;
+
+import org.mule.config.dsl.Module;
+
+import java.util.List;
+
+public interface DSLMultiModulesDescriptor {
+
+    List<Module> modules();
+
+}
---
+++ api/src/main/java/org/mule/config/dsl/descriptor/DSLSingleModuleDescriptor.java
@@ -0,0 +1,9 @@
+package org.mule.config.dsl.descriptor;
+
+import org.mule.config.dsl.Module;
+
+public interface DSLSingleModuleDescriptor {
+
+    Module module();
+
+}
